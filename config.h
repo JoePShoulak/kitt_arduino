5c779280768a3e3286c27665bb19a3017b7e3fdd
Joe P Shoulak <1021647+JoePShoulak@users.noreply.github.com>
2025-06-09 01:34:28 -0500

    calling it a night

--- config.h
+++ config.h
@@ -10,25 +10,25 @@
 void null_btn(lv_event_t *e); 
 
 const ButtonData button_panel1[BUTTON_COUNT] = {
-    { "TURBO BOOST", null_btn, true, true, true },
-    { "MAP SYSTEM", null_btn, true, false },
-    { "SKI MODE", null_btn, false, true },
-    { "VOLTAGE OUTPUT", null_btn, true, false },
-    { "VITAL SCAN", null_btn, false, false },
-    { "EVADE", null_btn, false, false },
-    { "RANGE BRITE", null_btn, false, false },
-    { "RADAR IMAGE", null_btn, false, false },
+    { "TURBO BOOST", null_btn, true, 1000, true },
+    { "MAP SYSTEM", null_btn, true, 0 },
+    { "PRINTER", null_btn, true, 0 },
+    { "VOLTAGE OUTPUT", null_btn, true, 0 },
+    { "VITAL SCAN", null_btn, false, 0 },
+    { "EVADE", null_btn, false, 0 },
+    { "RANGE BRITE", null_btn, false, 0 },
+    { "RADAR IMAGE", null_btn, false, 0 },
 };
 
 const ButtonData button_panel2[BUTTON_COUNT] = {
-    { "BAD DOLPHINS", null_btn, true, true, true },
-    { "NERVE GAS", null_btn, true, false },
-    { "SHARKS", null_btn, false, true },
-    { "CUTE OTTERS", null_btn, true, false },
-    { "EMERGENCY METH", null_btn, false, false },
-    { "SCARLETT JOHANSSON", null_btn, true, true, true },
-    { "BROWNIES", null_btn, false, false },
-    { "POT BROWNIES", null_btn, false, false },
+    { "MOTOR", null_btn, true, 1000, true },
+    { "BLACKOUT", null_btn, true, 1000 },
+    { "24V MODE", null_btn, true, 1000 },
+    { "INVERTER", null_btn, true, 1000 },
+    { "GPS", null_btn, true, 0, true },
+    { "RADIO", null_btn, true, 0, true },
+    { "USB", null_btn, true, 0, true },
+    { "LIGHTING", null_btn, true, 0 },
 };
 
 // VIZ DATA
--- kitt.ino
+++ kitt.ino
@@ -11,7 +11,7 @@
 
 #include <GigaAudio.h>
 
-GigaAudio audio("USB DISK"); // replace with name of USB volume
+// GigaAudio audio("USB DISK"); // replace with name of USB volume
 
 GigaDisplay_GFX tft; // Init tft
 Arduino_GigaDisplayTouch TouchDetector;
@@ -43,25 +43,26 @@ void setup() {
   lv_obj_set_scrollbar_mode(tiles, LV_SCROLLBAR_MODE_OFF);
 
   make_panel(button_panel1, tiles, 0);
+  // TODO: Make the voice tile a class so we can store the visualiser and indicators
+  // and update them from the voice tile to reflect things like voice state, indicators, etc.
   lv_obj_t* voice_tile = create_voice_tile(tiles, 1, voice_buttons);
   make_panel(button_panel2, tiles, 2);
 
   lv_obj_set_tile_id(tiles, 1, 0, LV_ANIM_OFF); // start on voice tile
-  LV_UNUSED(voice_tile); 
 
-  if (!audio.load("shoe.wav")) {  // replace with name of file to play
-    if (audio.hasError()) Serial.println(audio.errorMessage());
-    else Serial.println("Cannot load WAV file");
-    return;
-  }
-  audio.play();
+  // if (!audio.load("shoe.wav")) {  // replace with name of file to play
+  //   if (audio.hasError()) Serial.println(audio.errorMessage());
+  //   else Serial.println("Cannot load WAV file");
+  //   return;
+  // }
+  // audio.play();
 
 }
 
 void loop() {
   lv_timer_handler();
-  if (audio.isFinished()) {
-    audio.play(); // restart the playback when it is complete
-    Serial.println("Restarting . . .");
-}
+//   if (audio.isFinished()) {
+//     audio.play(); // restart the playback when it is complete
+//     Serial.println("Restarting . . .");
+//    }
 }
