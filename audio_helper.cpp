4e880ccfecd66b8734de1e018b82b8f5e9db7dbc
Joe P Shoulak <1021647+JoePShoulak@users.noreply.github.com>
2025-06-09 15:33:58 -0500

    Fix audio initialization timing

--- audio_helper.cpp
+++ audio_helper.cpp
@@ -1,21 +1,23 @@
 #include "audio_helper.h"
 #include <Arduino.h>
 
-static GigaAudio audio("USB DISK");
+static GigaAudio *audio = nullptr;
 
 static const char *audio_files[] = {"intro.wav", "explode.wav", "shoe.wav"};
 static const int audio_file_count = sizeof(audio_files) / sizeof(audio_files[0]);
 static int current_audio_index = 0;
 
 void audio_setup() {
+    if (!audio)
+        audio = new GigaAudio("USB DISK");
     current_audio_index = 0;
     play_current_audio();
 }
 
 bool load_current_audio() {
-    if (!audio.load(const_cast<char *>(audio_files[current_audio_index]))) {
-        if (audio.hasError()) {
-            Serial.println(audio.errorMessage());
+    if (!audio || !audio->load(const_cast<char *>(audio_files[current_audio_index]))) {
+        if (audio && audio->hasError()) {
+            Serial.println(audio->errorMessage());
         } else {
             Serial.print("Cannot load WAV file ");
             Serial.println(audio_files[current_audio_index]);
@@ -26,28 +28,28 @@ bool load_current_audio() {
 }
 
 bool play_audio_file(const char *filename) {
-    if (!audio.load(const_cast<char *>(filename))) {
-        if (audio.hasError())
-            Serial.println(audio.errorMessage());
+    if (!audio || !audio->load(const_cast<char *>(filename))) {
+        if (audio && audio->hasError())
+            Serial.println(audio->errorMessage());
         else {
             Serial.print("Cannot load WAV file ");
             Serial.println(filename);
         }
         return false;
     }
-    audio.play();
+    audio->play();
     return true;
 }
 
 bool play_current_audio() {
     if (!load_current_audio())
         return false;
-    audio.play();
+    audio->play();
     return true;
 }
 
 void audio_loop() {
-    if (audio.isFinished()) {
+    if (audio && audio->isFinished()) {
         current_audio_index = (current_audio_index + 1) % audio_file_count;
         if (play_current_audio()) {
             Serial.println("Restarting . . .");
