714d25ddcaad42267b45b6017299bf4e90d3ae3b
Joe P Shoulak <1021647+JoePShoulak@users.noreply.github.com>
2025-06-09 22:27:53 -0500

    Enable large LVGL fonts

---
+++ lv_conf.h
@@ -0,0 +1,16 @@
+#if 1 /* Set this to "1" to enable content */
+#ifndef LV_CONF_H
+#define LV_CONF_H
+
+#define LV_FONT_MONTSERRAT_14 1
+#define LV_FONT_MONTSERRAT_20 1
+#define LV_FONT_MONTSERRAT_24 1
+#define LV_FONT_MONTSERRAT_28 1
+#define LV_FONT_MONTSERRAT_32 1
+
+#define LV_FONT_DEFAULT &lv_font_montserrat_14
+
+#include "lv_conf_internal.h"
+
+#endif /*LV_CONF_H*/
+#endif /*End of "Content enable"*/
