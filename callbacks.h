e37691417f068dd9639a7e8bcf81cdf5ebc7522e
Joe P Shoulak <1021647+JoePShoulak@users.noreply.github.com>
2025-06-17 18:00:20 -0500

    file keeps coming back

--- callbacks.h
+++ callbacks.h
@@ -3,19 +3,21 @@
 
 #include <lvgl.h>
 
+// Default button callback
 void null_btn(lv_event_t *e);
 
+// Left button panel
+void turbo_boost_btn_cb(lv_event_t *e);
+void theme_btn_cb(lv_event_t *e);
 void intro_btn_cb(lv_event_t *e);
 void explode_btn_cb(lv_event_t *e);
-void joseph_btn_cb(lv_event_t *e);
-void shoe_btn_cb(lv_event_t *e);
 void michelle_btn_cb(lv_event_t *e);
 void shawn_btn_cb(lv_event_t *e);
-void theme_btn_cb(lv_event_t *e);
-void turbo_boost_btn_cb(lv_event_t *e);
-void auto_cruise_btn_cb(lv_event_t *e);
-void normal_cruise_btn_cb(lv_event_t *e);
-void pursuit_btn_cb(lv_event_t *e);
+void joseph_btn_cb(lv_event_t *e);
+void shoe_btn_cb(lv_event_t *e);
+
+// Right Button Panel
+void motor_override_cb(lv_event_t *e);
 void evade_btn_cb(lv_event_t *e);
 void btn48v_cb(lv_event_t *e);
 void inverter_btn_cb(lv_event_t *e);
@@ -23,8 +25,14 @@ void gps_btn_cb(lv_event_t *e);
 void radio_btn_cb(lv_event_t *e);
 void usb_btn_cb(lv_event_t *e);
 void lighting_btn_cb(lv_event_t *e);
-void motor_override_cb(lv_event_t *e);
-void blackout_cb(lv_event_t *e);
+
+// Voice panel
 void voice_mode_cb(lv_event_t *e);
+void auto_cruise_btn_cb(lv_event_t *e);
+void normal_cruise_btn_cb(lv_event_t *e);
+void pursuit_btn_cb(lv_event_t *e);
+
+// Misc
+void blackout_cb(lv_event_t *e);
 
 #endif
